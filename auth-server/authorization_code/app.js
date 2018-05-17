db008c596345b8b0be5e92f5c3f1c9873f7c765b
Ali <ar.k@outlook.com>
2018-05-17 14:00:16 -0700

    delete clientId, clientSecret and redirectURI from the app.js for security

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'a1e9ed59c50443659d9d9d3220141953'; // Your client id
-var client_secret = 'ead250bf839f4ca48fe4eb1caa337b29'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+var client_id = 'CLIENT_ID'; // Your client id
+var client_secret = 'CLIENT_SECRET'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
